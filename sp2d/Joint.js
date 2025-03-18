d4baec9ce2fe2393fa7ed7aab256a4bd9b75a71b
Connor Yan <connoryan11@outlook.com>
2025-03-18 23:14:27 +1030

    1.2
    Spring Joint finished

--- sp2d/Joint.js
+++ sp2d/Joint.js
@@ -13,8 +13,8 @@ export class Joint {
      * @param {boolean} visibility
      * @param {string} colour
      */
-    constructor(bodyA, bodyB, anchorA, anchorB, visibility,colour="#000000") {
-        this.id=null;
+    constructor(bodyA, bodyB, anchorA, anchorB, visibility, colour = "#000000") {
+        this.id = null;
 
         this.bodyA = bodyA;
         this.anchorA = anchorA.clone();
@@ -23,15 +23,15 @@ export class Joint {
         this.anchorB = anchorB.clone();
         //anchor point on entity. The coordinates are relative to the object
 
-        this.visibility=visibility;
+        this.visibility = visibility;
         //draw or not
 
-        this.colour=colour;
+        this.colour = colour;
     }
 
-    apply() {}
+    apply() { }
 
-    draw() {}
+    draw() { }
 }
 
 export class Distance extends Joint {
@@ -42,46 +42,37 @@ export class Distance extends Joint {
      * @param {Entity.Circle|Entity.Rectangle|Entity.Polygon} bodyA
      * @param {Entity.Circle|Entity.Rectangle|Entity.Polygon} bodyB
      */
-    constructor(bodyA, bodyB, anchorA, anchorB, visibility=false, colour) {
+    constructor(bodyA, bodyB, anchorA, anchorB, visibility = false, colour) {
         super(bodyA, bodyB, anchorA, anchorB, visibility, colour);
         // this.length = anchorA.subtract(anchorB).length();
-        this.id=null;
+        this.id = null;
         this.length = anchorA.add(bodyA.pos).subtract(anchorB.add(bodyB.pos)).length();
     }
 
     apply() {
         // a joint is actually a collision
-        var corrFactor = 0.3/PhysicsScene.substep;
+        var corrFactor = 0.3 / PhysicsScene.substep;
 
-        var b=this.bodyA;
-        var a=this.bodyB;
+        var b = this.bodyA;
+        var a = this.bodyB;
         var Pa = this.bodyA.pos.add(this.anchorA);
         //Absolute coordinates of unconstrained entity
         var Pb = this.bodyB.pos.add(this.anchorB);
-        
+
         var diff = Pb.subtract(Pa);
         var currentLength = diff.length();
-
         var normal = diff.normalise();
         var depth = currentLength - this.length;
 
         var e = 0;
-
         var r_ap = Pa.subtract(a.pos);
         var r_bp = Pb.subtract(b.pos);
-
         var v_a = a.vel.add(r_ap.perpendicular(), a.angularVel);
         var v_b = b.vel.add(r_bp.perpendicular(), b.angularVel);
-
-        //console.log(b.angularVel);
-
         var v_rel = v_b.subtract(v_a);
         var v_rel_n = v_rel.dot(normal);
 
-        if(v_rel_n>0)
-        {
-            return;
-        }//seperating
+        if (v_rel_n > 0) { return; }//seperating
 
         var r_ap_cross_n = r_ap.cross(normal);
         var r_bp_cross_n = r_bp.cross(normal);
@@ -122,5 +113,71 @@ export class Distance extends Joint {
         var worldAnchorA = this.bodyA.pos.add(this.anchorA);
         var worldAnchorB = this.bodyB.pos.add(this.anchorB);
         Draw.drawSpring(worldAnchorA, worldAnchorB, 1, 0, this.colour); //straight line
+    }
+}
+
+export class Spring extends Joint {
+    /**
+     * @param {Entity.Circle|Entity.Rectangle|Entity.Polygon} bodyA
+     * @param {Entity.Circle|Entity.Rectangle|Entity.Polygon} bodyB
+     * @param {Vector2} anchorA
+     * @param {Vector2} anchorB
+     * @param {number} stiffness
+     * @param {number} damping
+     * @param {boolean} visibility
+     * @param {string} colour
+     */
+    constructor(bodyA, bodyB, anchorA, anchorB, width, stiffness, damping, visibility = false, colour = "#000000") {
+        super(bodyA, bodyB, anchorA, anchorB, visibility, colour);
+        this.id = null;
+        this.width=width;
+        this.stiffness = stiffness; //aka spring constant
+        this.damping = damping; //you know what it is
+        this.restLength = anchorA.add(bodyA.pos).subtract(anchorB.add(bodyB.pos)).length(); // 计算自然长度
+    }
+
+    apply() {
+        var dt=PhysicsScene.dt/PhysicsScene.substep;
+        var a = this.bodyA;
+        var b = this.bodyB;
+        
+        var worldAnchorA = a.pos.add(this.anchorA);
+        var worldAnchorB = b.pos.add(this.anchorB);
+        var diff = worldAnchorB.subtract(worldAnchorA);
+
+        var currentLength = diff.length();
+        if (currentLength === 0) return;
+
+        var normal = diff.normalise();
+        var stretch = currentLength - this.restLength;
+
+        // Hooke's law
+        var springForce = normal.times(-this.stiffness * stretch*dt);
+
+        var r_ap = worldAnchorA.subtract(a.pos);
+        var r_bp = worldAnchorB.subtract(b.pos);
+
+        var v_a = a.vel.add(r_ap.perpendicular().times(a.angularVel));
+        var v_b = b.vel.add(r_bp.perpendicular().times(b.angularVel));
+
+        var v_rel = v_b.subtract(v_a);
+        var dampingForce = normal.times(-this.damping * dt * v_rel.dot(normal));
+
+        var force = springForce.add(dampingForce);
+
+        if (a.mass !== Infinity) {
+            a.vel.addEqual(force.times(-a.massInv));
+            a.angularVel -= r_ap.cross(force) * a.inertiaInv;
+        }
+        if (b.mass !== Infinity) {
+            b.vel.addEqual(force.times(b.massInv));
+            b.angularVel += r_bp.cross(force) * b.inertiaInv;
+        }
+    }
+
+    draw() {
+        var worldAnchorA = this.bodyA.pos.add(this.anchorA);
+        var worldAnchorB = this.bodyB.pos.add(this.anchorB);
+        Draw.drawSpring(worldAnchorA, worldAnchorB, 10,this.width, this.colour); 
     }
 }
---
+++ spring.js
@@ -0,0 +1,84 @@
+import { Vector2, VectorMath2 } from "./sp2d/Vector2.js";
+import * as Draw from "./sp2d/Draw.js";
+import * as PhysicsScene from "./sp2d/PhysicsScene.js";
+import * as Entity from "./sp2d/Entity.js";
+import * as Collision from "./sp2d/Collision.js";
+import * as Joint from "./sp2d/Joint.js";
+
+var div = 2;
+
+function setupScene() {
+    Draw.init("myCanvas", Math.min(window.innerWidth - 20, window.innerHeight - 20), Math.min(window.innerWidth - 20, window.innerHeight - 20), 2);
+    PhysicsScene.init(undefined, new Vector2(0.0, -9.8), 0.1);
+}
+window.setupScene = setupScene;
+
+/**
+ * @param {Vector2} pos
+ */
+function addPendulum(pos)
+{
+    var hinge=new Entity.Circle(0.05, 0, 0, Infinity,
+        pos,
+        VectorMath2.zero(),
+        0,
+        0,
+        "#3f3f3f"
+    )
+    hinge.setStatic();
+    PhysicsScene.addEntity(hinge);
+
+    var length=1;
+
+    var ball=new Entity.Circle(0.1, 1, 0, undefined,
+        pos.add(new Vector2(0,-length)),
+        VectorMath2.zero(),
+        0,
+        0,
+        "#9F0000"
+    )
+    var id=PhysicsScene.addEntity(ball);
+
+    var rope=new Joint.Spring(hinge, ball,
+        VectorMath2.zero(),
+        VectorMath2.zero(),
+        0.05,
+        1,
+        0.01,
+        true,
+        "#111111"
+    );
+    PhysicsScene.addJoint(rope);
+    return id;
+}
+
+function hangPendulum()
+{
+    var mid=new Vector2(1,1.5);
+    var plug=addPendulum(mid);
+
+    // var diff=new Vector2(0.2,0);
+
+    // for(let i=1;i<=2;i++)
+    // {
+    //     addPendulum(mid.add(diff.times(i)));
+    //     plug=addPendulum(mid.add(diff.times(-i)));
+    // }
+    // //console.log(PhysicsScene.entities);
+
+    PhysicsScene.entities[plug].pos.subtractEqual(new Vector2(0,0));
+}
+
+function updateFrame()
+{
+    //console.log(PhysicsScene.entities);
+    PhysicsScene.simulate(100);
+    PhysicsScene.draw();
+    PhysicsScene.drawVertex();
+    PhysicsScene.drawContactPoint();
+    requestAnimationFrame(updateFrame);
+}
+
+setupScene();
+hangPendulum();
+updateFrame();
