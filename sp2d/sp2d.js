8ba6f94923f0d8538648c7c0f354caf68abbe89d
Connor Yan <connoryan11@outlook.com>
2025-03-22 01:07:11 +1030

    2.0
    Index file sp2d.js.

---
+++ sp2d/sp2d.js
@@ -0,0 +1,8 @@
+import { Vector2, VectorMath2 } from "./Vector2.js";
+import * as Draw from "./Draw.js";
+import * as PhysicsScene from "./PhysicsScene.js";
+import * as Entity from "./Entity.js";
+import * as Collision from "./Collision.js";
+import * as Joint from "./Joint.js";
+
+export{ Vector2, VectorMath2, Draw, PhysicsScene, Entity, Collision, Joint};
