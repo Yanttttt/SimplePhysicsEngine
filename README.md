2610106217aa399fd45ddbedb9c7e93e468df067
Connor Yan <connoryan11@outlook.com>
2025-03-23 22:24:34 +1030

    2.2
    Update Readme.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 This is SACE Year 12 Digital Tech Individual Digital Solution.
  
+The program must be run in an HTTP/HTTPS server environment. If you want to run the code locally, you can try using VS Code + Live Server, or use Python's built-in server.
--- sp2d/sp2d.js
+++ sp2d/sp2d.js
@@ -5,4 +5,7 @@ import * as Entity from "./Entity.js";
 import * as Collision from "./Collision.js";
 import * as Joint from "./Joint.js";
 
+// import statement only works in an HTTP/HTTPS server environment
+// I use VS Code  Extension "Live server" to simulate that.
+
 export{ Vector2, VectorMath2, Draw, PhysicsScene, Entity, Collision, Joint};
